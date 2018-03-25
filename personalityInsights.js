52973d04935751054676dda8a645a257133a2526
Opesanya Adebayo <bayoopesanya@gmail.com>
2018-03-25 23:04:44 +0100

    added textual summary

--- personalityInsights.js
+++ personalityInsights.js
@@ -1,10 +1,16 @@
 const readline = require('readline');
-const PersonalityInsightsV3 =require('watson-developer-cloud/personality-insights/v3');
+const PersonalityInsightsV3 = require('watson-developer-cloud/personality-insights/v3');
 const personality_insights = new PersonalityInsightsV3({
-    username: process.env.PERSONALITY_INSIGHTS_USERNAME,
-    password: process.env.PERSONALITY_INSIGHTS_PASSWORD,
-    version_date: process.env.PERSONALITY_INSIGHTS_VERSION_DATE
+  username: process.env.PERSONALITY_INSIGHTS_USERNAME,
+  password: process.env.PERSONALITY_INSIGHTS_PASSWORD,
+  version_date: process.env.PERSONALITY_INSIGHTS_VERSION_DATE
 });
+const PersonalityTextSummaries = require('personality-text-summary');
+const v3EnglishTextSummaries = new PersonalityTextSummaries({
+  locale: 'en',
+  version: 'v3'
+});
+
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -20,13 +26,22 @@ rl.question('Please enter a short paragraph for Watson to analyze...', (text) =>
     consumption_preferences: true
   };
 
-  personality_insights.profile(params, function(error, response) {
-  if (error)
-    console.log('Error:', error);
-  else
-    console.log(JSON.stringify(response, null, 2));
-  }
-);
+  personality_insights.profile(params, function (error, response) {
+    if (error)
+      console.log('Error:', error);
+    else
+      console.log(getTextSummary(response));
+    //console.log(JSON.stringify(response, null, 2));
+  });
 
   rl.close();
 });
+
+const getTextSummary = personalityProfile => {
+  let textSummary = v3EnglishTextSummaries.getSummary(personalityProfile);
+  if (typeof (textSummary) !== 'string') {
+    console.log("Could not get summary.");
+  } else {
+    return textSummary;
+  }
+};
